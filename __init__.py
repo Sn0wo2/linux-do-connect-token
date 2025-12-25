039ee0b4083f37df147650c82ae1bb3840d260aa
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-12-25 22:07:25 +0800

    chore(poetry): remove version & fix exports

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 """Linux Do Connect Token - A helper library to authenticate with connect.linux.do and retrieve auth.session-token."""
 
-__version__ = "0.0.0"
+from .linux_do_connect import LinuxDoConnect
 
-from .linux_do_connect import LinuxDoConnect, get_auth_session, get_token
-
-__all__ = ["LinuxDoConnect", "get_auth_session", "get_token"]
+__all__ = ["LinuxDoConnect"]
